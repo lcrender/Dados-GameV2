47d30f648ae9cafc50a5f047b6c030a2b6af5ac2
Alejandro Chazarreta <lcrender@gmail.com>
2023-02-09 17:49:12 +0100

    clean code

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -4,39 +4,51 @@ const { JWTSECRET } = process.env;
 const authCtrl = {};
 
 authCtrl.signUp = async (req, res) => {
-    const { email, password} = req.body;
-    const user = new User({
-        email,
-        password
-    });
-    user.password = await user.encryptPassword(user.password)
-    await user.save();
-    const token = jwt.sign({id: user._id}, JWTSECRET, {
-        expiresIn: 60 * 60 * 1
-    })
-    res.status(201).json({auth: true, token});
+	try {
+		const { email, password } = req.body;
+		const user = new User({
+			email,
+			password
+		});
+		user.password = await user.encryptPassword(user.password);
+		await user.save();
+		const token = jwt.sign({ id: user._id }, JWTSECRET, {
+			expiresIn: 60 * 60 * 1
+		});
+		res.status(201).json({ auth: true, token });
+	} catch (error) {
+		res.status(401).json(error);
+	}
 };
 authCtrl.logIn = async (req, res) => {
-    const {email, password} = req.body;
-    const user = await User.findOne({email: email})
-    if (!user) {
-        return res.status(404).send("The email doesn't exists");
-    } 
-    const validPassword = await user.validatePassword(password);
-    if (!validPassword) {
-       return res.status(401).json({auth: false, token: null});
-    }
-    const token = jwt.sign({id: user._id}, JWTSECRET, {
-        expiresIn: 60 * 60 * 1
-    })
-    res.status(201).json({auth: true, token}); 
+	try {
+		const { email, password } = req.body;
+		const user = await User.findOne({ email: email });
+		if (!user) {
+			return res.status(404).send("The email doesn't exists");
+		}
+		const validPassword = await user.validatePassword(password);
+		if (!validPassword) {
+			return res.status(401).json({ auth: false, token: null });
+		}
+		const token = jwt.sign({ id: user._id }, JWTSECRET, {
+			expiresIn: 60 * 60 * 1
+		});
+		res.status(201).json({ auth: true, token });
+	} catch (error) {
+		res.status(401).json(error);
+	}
 };
 authCtrl.viewMe = async (req, res) => {
-    const user = await User.findById(req.userId, { password: 0 });
-    if (!user) {
-        return res.status(404).send('No user found');
-    }
-    res.status(201).json(user)
+	try {
+		const user = await User.findById(req.userId, { password: 0 });
+		if (!user) {
+			return res.status(404).send('No user found');
+		}
+		res.status(201).json(user);
+	} catch (error) {
+		res.status(401).json(error);
+	}
 };
 
 module.exports = authCtrl;
--- app/controllers/game.controller.js
+++ app/controllers/game.controller.js
@@ -2,55 +2,75 @@ const Player = require('../models/Player');
 const RollGame = require('../models/Games');
 const gameCtrl = {};
 
-gameCtrl.playRollDice = async (req, res) => {   
-    const id = req.params.id;
-    const game = await new RollGame(id);
-    const playerRollDices = await game.playRollGame();
-    res.status(201).json(playerRollDices)
+gameCtrl.playRollDice = async (req, res) => {
+	try {
+		const id = req.params.id;
+		const game = await new RollGame(id);
+		const playerRollDices = await game.playRollGame();
+		res.status(201).json(playerRollDices);
+	} catch (error) {
+		res.status(401).json(error);
+	}
 };
 gameCtrl.generalRanking = async (req, res) => {
-    const players = await Player.find({}, {playHistory: 0, _id: 0, __v: 0}).sort({wonRate: -1});
-    let totalWrate = players.reduce((acumulador, actual) => acumulador + actual.wonRate, 0);
-    totalWrate = totalWrate / players.length
-    const msg = `Total Won Rate: ${totalWrate}`
-    res.status(201).json({msg, players})
+	try {
+		const players = await Player.find({}, { playHistory: 0, _id: 0, __v: 0 }).sort({ wonRate: -1 });
+		let totalWrate = players.reduce((acumulador, actual) => acumulador + actual.wonRate, 0);
+		totalWrate = totalWrate / players.length;
+		const msg = `Total Won Rate: ${totalWrate}`;
+		res.status(201).json({ msg, players });
+	} catch (error) {
+		res.status(401).json(error);
+	}
 };
 gameCtrl.getBetterPlayer = async (req, res) => {
-    const players = await Player.find({})
-    let max = 0;
-    players.forEach(player => {
-        player.wonRate > max ? max = player.wonRate : null
-    })
-    const betterPlayer = await Player.findOne({wonRate: max}, {_id: 0, __v: 0});
-    res.status(201).json({betterPlayer})
+	try {
+		const players = await Player.find({});
+		let max = 0;
+		players.forEach((player) => {
+			player.wonRate > max ? (max = player.wonRate) : null;
+		});
+		const betterPlayer = await Player.findOne({ wonRate: max }, { _id: 0, __v: 0 });
+		res.status(201).json({ betterPlayer });
+	} catch (error) {
+		res.status(401).json(error);
+	}
 };
 gameCtrl.getWorstPlayer = async (req, res) => {
-    const players = await Player.find({})
-        let min = 100
-        players.forEach(player => {
-            player.wonRate < min ? min = player.wonRate : null
-        })
-        const worstPlayer = await Player.findOne({wonRate: min}, {_id: 0, __v: 0});
-    res.status(201).json({worstPlayer})
+	try {
+		const players = await Player.find({});
+		let min = 100;
+		players.forEach((player) => {
+			player.wonRate < min ? (min = player.wonRate) : null;
+		});
+		const worstPlayer = await Player.findOne({ wonRate: min }, { _id: 0, __v: 0 });
+		res.status(201).json({ worstPlayer });
+	} catch (error) {
+		res.status(401).json(error);
+	}
 };
 gameCtrl.deleteGames = async (req, res) => {
-    const player = await Player.findById(req.params.id);
-    player.totalGames = 0
-    player.gamesWon = 0
-    player.wonRate = 0
-    player.playHistory = []
-    await player.save()
-    res.status(201).json(player)
+	try {
+		const player = await Player.findById(req.params.id);
+		player.totalGames = 0;
+		player.gamesWon = 0;
+		player.wonRate = 0;
+		player.playHistory = [];
+		await player.save();
+		res.status(201).json(player);
+	} catch (error) {
+		res.status(401).json(error);
+	}
 };
 gameCtrl.viewGames = async (req, res) => {
-    try {
-        const player = await Player.findById(req.params.id, { playHistory:1, username: 1, _id: 0 });
-        if (!player) {
-            return res.status(404).send('No player found');
-        }
-        res.status(201).json(player)
-    } catch (error) {
-        res.send(error)
-    }
+	try {
+		const player = await Player.findById(req.params.id, { playHistory: 1, username: 1, _id: 0 });
+		if (!player) {
+			return res.status(404).send('No player found');
+		}
+		res.status(201).json(player);
+	} catch (error) {
+		res.status(401).json(error);
+	}
 };
-module.exports = gameCtrl
+module.exports = gameCtrl;
