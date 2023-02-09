152b5a3b42d6b61ebdac9ff802b556607952842e
Alejandro Chazarreta <lcrender@gmail.com>
2023-02-09 17:30:47 +0100

    fix

--- app/routes/index.routes.js
+++
@@ -1,6 +0,0 @@
-const { Router } = require('express');
-const router = Router();
-const { indexRender } = require('../controllers/index.controller');
-router.get('/', indexRender);
-
-module.exports = router;
--- app/server.js
+++ app/server.js
@@ -2,12 +2,11 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 // Settings
-app.set('port', process.env.PORT || 4000);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 // Routes
-app.use(require('./routes/index.routes'));
 app.use(require('./routes/auth.routes'));
 app.use(require('./routes/game.routes'));
 app.use(require('./routes/player.routes'));
